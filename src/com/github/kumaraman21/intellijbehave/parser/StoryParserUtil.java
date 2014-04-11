1162c2e3a7c24001d45eae84417cf1be676a816d
Dmitry Malchikov <dmalch@gmail.com>
2014-04-11 11:24:25 +0200

    Reworked generation. Gen source root added to the source roots.

---
+++ src/com/github/kumaraman21/intellijbehave/parser/StoryParserUtil.java
@@ -0,0 +1,6 @@
+package com.github.kumaraman21.intellijbehave.parser;
+
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+public class StoryParserUtil extends GeneratedParserUtilBase {
+}
---
+++ src/com/github/kumaraman21/intellijbehave/psi/impl/StoryPsiImplUtil.java
@@ -0,0 +1,4 @@
+package com.github.kumaraman21.intellijbehave.psi.impl;
+
+public class StoryPsiImplUtil {
+}
